fb191535cfc7da530f9e7075e02fc4f051723bc7
masao-okamoto <masao.okamoto@dena.com>
2017-11-09 12:35:49 +0900

    fetch api

--- src/Main.js
+++ src/Main.js
@@ -4,14 +4,18 @@ import { Switch, Route } from 'react-router-dom';
 import TimelineApp from './TimelineApp.js';
 import About from './About.js';
 import Hello from './Hello.js';
+import Human from './Human.js';
 
 class Main extends Component {
     render() {
         return (
             <div className="Main">
-                <Route exact path='/' component={TimelineApp} />
-                <Route exact path='/about/:number' component={About} />
-                <Route exact path='/hello' component={Hello} />
+                <Switch>
+                    <Route exact path='/' component={TimelineApp} />
+                    <Route exact path='/about/:number' component={About} />
+                    <Route exact path='/hello' component={Hello} />
+                    <Route exact path='/human' component={Human} />
+                </Switch>
             </div>
         );
     }
--- src/action.js
+++ src/action.js
@@ -1,3 +1,5 @@
+import { store } from './store.js';
+
 export function mapStateToProps(state) {
     return state;
 }
@@ -10,6 +12,20 @@ export function mapDispatchToProps(dispatch) {
         changeText: (text) => {
             dispatch({type:'CHANGE_TEXT', text: text});
         },
+        searchHuman: () => {
+            let f = function(dispatch) {
+                return fetch('http://localhost:3000/test.json').then(
+                    response => {
+                        response.json().then(
+                            json => dispatch({type: 'HUMAN', human: json})
+                        ),
+                        error => dispatch({type: 'HUMAN_ERROR', reason: error})
+                    }
+                )
+            }
+
+            store.dispatch(f).then( () => { console.log("Done!"); } );
+        }
 
     };
 }
